e18bcaa70d7157bbc3b58fe18c31294009611466
Stuart Halloway <stu@cognitect.com>
2018-11-07 19:58:05 -0500

    support all the xy-chart data shapes, fixed to bar chart for now

--- src/cognitect/rebl/charts.clj
+++ src/cognitect/rebl/charts.clj
@@ -14,6 +14,92 @@
   [container data]
   (.setAll container (fx/fxlist data)))
 
+(defn distinct-values
+  "Returns the set of distinct values of (f x) where x is sampled
+from coll."
+  [coll f]
+  (->> (fx/sample coll fx/coll-check-limit)
+       (map f)
+       (into #{})))
+
+(defn scalar-type
+  [x]
+  (cond
+   (number? x) :num
+   (string? x) :name
+   (keyword? x) :name
+   (symbol? x) :name))
+
+(defn- sample [coll] (fx/sample coll fx/coll-check-limit))
+(defn- mostly? [pred coll] (every? pred (sample coll)))
+
+(defn twople?
+  [x]
+  (= 2 (bounded-count 2 x)))
+
+(defn coll-of-nums?
+  [x]
+  (and (instance? java.util.Collection x)
+       (mostly? number? x)))
+
+(defn scalar-num-pair?
+  [x]
+  (and (instance? java.util.Collection x)
+       (twople? x)
+       (scalar-type (first x))
+       (number? (second x))))
+
+(defn scalar->num?
+  [x]
+  (and (instance? java.util.Map x)
+       (mostly? scalar-num-pair? x)))
+
+(defn coll-of-pairs?
+  [x]
+  (and (instance? java.util.Collection x)
+       (mostly? scalar-num-pair? x)))
+
+(defn pair-of-colls?
+  [x]
+  (and (instance? java.util.Collection x)
+       (twople? x)
+       (instance? java.util.Collection (first x))
+       (instance? java.util.Collection (second x))
+       (mostly? scalar-type (first x))
+       (mostly? number? (second x))))
+
+(defn xy-type
+  [x]
+  (cond
+   (coll-of-nums? x) :coll-of-nums
+   (scalar->num? x) :scalar->num
+   (coll-of-pairs? x) :coll-of-pairs
+   (pair-of-colls? x) :pair-of-colls))
+
+(defn xy-chartable?
+  [x]
+  (boolean (xy-type x)))
+
+(defmulti to-xy-series-data (fn [data] (xy-type data)))
+(defmethod to-xy-series-data :coll-of-nums [x] (map-indexed vector x))
+(defmethod to-xy-series-data :coll-of-pairs [x] x)
+(defmethod to-xy-series-data :scalar->num [x] x)
+(defmethod to-xy-series-data :pair-of-colls [[xs ys]] (map vector xs ys))
+
+(defn x-axis-for
+  [type]
+  (if (contains? {:bar :stacked-bar} type)
+    (CategoryAxis.)
+    (NumberAxis.)))
+
+(defn transform-x-axis
+  [type xy-series-data]
+  (if (contains? {:bar :stacked-bar} type)
+    (map (fn [[x y]]
+           [(str x) y])
+         xy-series-data)
+    xy-series-data))
+
 (defn- xy-data
   [{:keys [convx convy flipped?]} [x y extra :as v]]
   (let [x ((or convx identity) x)
@@ -23,20 +109,6 @@
           2 (XYChart$Data. x y)
           3 (XYChart$Data. y x extra))))
 
-;; TODO: update to current API and make live
-#_(defn histogram
-  [coll]
-  (let [x (CategoryAxis.)
-        y (NumberAxis.)
-        bc (BarChart. x y)
-        ser (XYChart$Series.)]
-    (.setLabel x "Range")
-    (.setLabel y "Value")
-    (.setName ser "Histogram")
-    (-> ser .getData (set-all (map xy-data [["a" 1] ["b" 2]])))
-    (-> bc .getData (set-all [ser]))
-    bc))
-
 (defn xy-series
   [{:keys [name] :as opts} tuples]
   (let [ser (XYChart$Series.)]
@@ -44,62 +116,11 @@
     (-> ser .getData (set-all (map #(xy-data opts %) tuples)))
     ser))
 
-;; TODO: infer from data, not from chart type
-(defn x-axis-for
-  [chart]
-  (if (get #{:bar :stacked-bar} chart)
-    (CategoryAxis.)
-    (NumberAxis.)))
-
-(defn sampled-set
-  [x f]
-  (->> (fx/sample x fx/coll-check-limit)
-       (map f)
-       frequencies
-       keys
-       (into #{})))
-
-(defn xy-type
-  [x]
-  (cond
-   (number? x) :num
-   (string? x) :category
-   (keyword? x) :category
-   (symbol? x) :category
-   (fx/Coll? x) (let [topcount (bounded-count 4 x)
-                      subtype (sampled-set x xy-type)]
-                  (if (or (= #{:coll-of-nums} subtype)
-                          (= #{:coll-of-nums :coll-of-categories} subtype))
-                    (let [subcounts (sampled-set x count)]
-                      ;; 2/3 x 2/3 colls are ambiguous, making a choice
-                      #_(prn {:topcount topcount :subtype subtype :subcounts subcounts})
-                      (cond (= #{2} subcounts) :coll-of-pairs
-                            (= #{3} subcounts) :coll-of-triples
-                            (= 2 topcount) :pair-of-colls
-                            (= 3 topcount) :triple-of-colls))
-                    (cond (= #{:num} subtype) :coll-of-nums
-                          (= #{:category} subtype) :coll-of-categories
-                          (= #{:num :category} subtype) :coll-of-vals)))))
-
-;; TODO  :coll-of-categories :pair-of-colls :triple-of-colls :coll-of-triples
-(defn xy-chartable?
-  [x]
-  (contains? #{:coll-of-nums :coll-of-pairs} (xy-type x)))
-
-(defmulti to-xy-series-data (fn [options data] (xy-type data)))
-
-(defmethod to-xy-series-data :coll-of-nums
-  [opts x]
-  (xy-series opts (map-indexed vector x)))
-
-(defmethod to-xy-series-data :coll-of-pairs
-  [opts x]
-  (xy-series opts x))
-
-(defn series-pair-chart
+(defn xy-chart
   [{:keys [x-label y-label type flipped?] :or {x-label "x" y-label "y"}}
    serieses]
-  (let [x (doto (x-axis-for type) (.setLabel x-label))
+  (let [xy-datas (map to-xy-series-data serieses)
+        x (doto (x-axis-for type) (.setLabel x-label))
         y (doto (NumberAxis.) (.setLabel y-label))
         [x y] (if flipped? [y x] [x y])
         c (case type
@@ -109,36 +130,25 @@
                 :bar (BarChart. x y)
                 :stacked-bar (StackedBarChart. x y)
                 :stacked-area (StackedAreaChart. x y))]
-    (-> c .getData (set-all (map (fn [[name data]]
-                                   (to-xy-series-data {:name name :flipped? flipped?} data))
-                                 serieses)))
+    (-> c .getData (set-all (map (fn [xy-data]
+                                   (->> xy-data
+                                        (transform-x-axis type)
+                                        (xy-series {:name "Data" :flipped? flipped?})))
+                                 xy-datas)))
     c))
 
-(defn chart-changed
-  [node coll]
-  (let [type (.getValue (node "chartChoice"))
-        flipped? (.isSelected (node "flippedButton"))
-        chart (series-pair-chart {:type type
-                                  :flipped? flipped?}
-                                 [["Data" coll]])]
-    (ui/update-pane (node "chartView") chart)))
-
-(defn series-pair-chart-v
+(defn xy-chart-v
   [coll]
   (let [loader (FXMLLoader. (io/resource "cognitect/rebl/series-pair-chart.fxml"))
         root (.load loader)
         names (.getNamespace loader)
         node (fn [id] (.get names id))
-        chart-choice (node "chartChoice")
-        flipped-button (node "flippedButton")
         pane (node "chartView")
-        changed (fx/change-listener (fn [_ _ _] (chart-changed node coll)))]
-    (-> chart-choice .valueProperty (.addListener changed))
-    (-> flipped-button .selectedProperty (.addListener changed))
-    (ui/update-choice chart-choice [:area :line :scatter] :scatter)
+        chart (xy-chart {:type :bar} [coll])]
+    (ui/update-pane pane chart)
     root))
 
-(rebl/update-viewers {:charts/series-pair {:pred xy-chartable? :ctor series-pair-chart-v}})
+(rebl/update-viewers {:rebl/xy-chart {:pred xy-chartable? :ctor xy-chart-v}})
 
 
 
