f5a9d6bda4682807f4d1b6b5c3e293000ff24b40
Stuart Halloway <stu@cognitect.com>
2018-11-20 14:04:46 -0500

    build a zip

--- build/ci.clj
+++ build/ci.clj
@@ -5,11 +5,21 @@
 (project/load-nses)
 
 (def project
-  (project/create {:lib 'com.datomic/rebl
+  (project/create {:lib 'com.cognitect/rebl
                    :version (str "0.9." (git/revision))
                    :namespace 'cognitect.rebl
                    :instrument? #(str/starts-with? (namespace %) "cognitect")
                    :project-class? #(str/starts-with? % "cognitect/rebl")}))
+
+(defn write-zip
+  "Write a zip file next to local-jar-path, containing the jar plus
+the contents of zip-static directory."
+  [{:keys [local-jar-path]}]
+  (let [jar-name (.getName (io/file local-jar-path))
+        zip-path (str/replace local-jar-path ".jar" ".zip")]
+    (with-open [zo (zip/output-stream zip-path)]
+      (zip/add-file-entry zo jar-name local-jar-path)
+      (zip/add-dir zo nil "zip-static"))))
 
 (defn local-build
   [project]
@@ -22,6 +32,7 @@
 (defn build
   [project]
   (local-build project)
+  (write-zip project)
   (project/deploy project))
 
 (defn -main [& _] (project/main build project))
