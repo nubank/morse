401ea5cf81d8f157e5f26636e3f7860157dfbda8
Stuart Halloway <stu@cognitect.com>
2019-02-12 11:31:39 -0500

    don't interpret java.lang code as beans

--- src/cognitect/rebl/impl/beans.clj
+++ src/cognitect/rebl/impl/beans.clj
@@ -6,13 +6,17 @@
   (:require [clojure.datafy :as datafy]
             [clojure.reflect :as reflect]))
 
+(def skip-package-set
+  #{(.getPackage String)})
+
 (defn- bean-class?*
   [c]
   (let [{:keys [bases members]} (datafy/datafy c)
         typesym (and c (@#'reflect/typesym c))
         constructors (get members typesym)]
     (and (contains? bases 'java.io.Serializable)
-         (some #(empty? (:parameter-types %)) constructors))))
+         (some #(empty? (:parameter-types %)) constructors)
+         (not (contains? skip-package-set (.getPackage c))))))
 
 (def bean-class? (memoize bean-class?*))
 
