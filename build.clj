5064f75c5183e7226dc807f75f9c9242e49040f4
fogus <mefogus@gmail.com>
2023-04-12 09:01:39 -0400

    adding dev-tools to artifact group id

--- build.clj
+++ build.clj
@@ -1,7 +1,7 @@
 (ns build
   (:require [clojure.tools.build.api :as b]))
 
-(def lib 'dev.nu/morse)
+(def lib 'dev.nu.dev-tools/morse)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
