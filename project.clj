de5380c6ededdae7f4812ca71ba432628c411954
fogus <mefogus@gmail.com>
2023-10-06 13:21:44 -0400

    Added a simple project.clj file for use in packaging

--- project.clj
+++ project.clj
@@ -1,3 +1,10 @@
 (defproject com.github.nubank/morse "master-SNAPSHOT"
   :description "A graphical, interactive tool for browsing Clojure data"
-  :dependencies [])
+  :dependencies [[org.clojure/core.async "1.5.648"]
+                 [org.openjfx/javafx-fxml     "19.0.2.1"]
+                 [org.openjfx/javafx-controls "19.0.2.1"]
+                 [org.openjfx/javafx-swing    "19.0.2.1"]
+                 [org.openjfx/javafx-base     "19.0.2.1"]
+                 [org.openjfx/javafx-web      "19.0.2.1"]
+                 [org.yaml/snakeyaml "1.23"]])
+
